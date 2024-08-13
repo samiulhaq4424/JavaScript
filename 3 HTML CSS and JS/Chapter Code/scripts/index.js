5294c0ec3a137abdd2594aa01a16b9b22e93e281
samiulhaq4424 <samiulhaq54424@gmail.com>
2024-08-13 19:02:21 +0530

    second-commit

--- 3 HTML CSS and JS/Chapter Code/scripts/index.js
+++ 3 HTML CSS and JS/Chapter Code/scripts/index.js
@@ -2,13 +2,13 @@
 alert("Hi sam!");
 
 /*here pop-up will not show but this message i.e hi bro..! will be there in console*/
-console.log("Hi, I am in console, meet me there.!");
+console.log("Hi, I am in console.!");
 
-/*here pop-up will not show but this message i.e This is a warning.. will be there in console in Warnong format*/
+/*here pop-up will not show but this message i.e This is a warning.. will be there in console in Warning format*/
 console.warn("This is a warning..");
 
 /*here pop-up will not show but this message i.e Stop. This is an error.. will be there in console 
-  in Warning format(generally in red text)*/
+  in error format(generally in red text)*/
 console.error("Stop. This is an error..");
 
 /*This method is used to clear the console, if niche ka comment removed then it will clear all the messgaes in console, that is why I have commented it */
